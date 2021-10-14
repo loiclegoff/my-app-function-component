61064e20138b5c804d65bf50cbb30823a3760fa1
Loïc Le Goff <dev.legoff@gmail.com>
2021-10-14 14:28:11 +0200

    ✨ feat(app): add handleMouseOverTitle on first component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import './lib/bootstrap-4.5.3-dist/css/bootstrap.min.css'
 function App(props) {
   // set the initial title value with injected props
   const [title, setTitle] = useState(props.title)
+  const [mouseOverNum, setMouseOverNum] = useState(0)
 
   //function with the event as parameter
   const handleChangeTitle = (e) => setTitle(e.target.value)
+  const handleMouseOverTitle = () => setMouseOverNum(mouseOverNum + 1)
 
   return (
     <div className="App">
       <h1>
         this is my first React Component{' '}
+        <span className='badge badge-primary'>
+          {mouseOverNum}
+        </span>
       </h1>
       <label htmlFor='titleInput'>Title</label>
       <input
@@ -21,7 +26,9 @@ function App(props) {
         onChange={handleChangeTitle}
         value={title}
       />
-      <h3>{title}</h3>
+      <div onMouseOver={handleMouseOverTitle}>
+        <h3>{title}</h3>
+      </div>
     </div>
   );
 }
